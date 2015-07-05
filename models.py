76465a6d4d35dd2b8fea4dfee60f78eb2e076dc7
Luis Lopez <lclg21@utexas.edu>
2015-07-04 22:33:34 -0500

    started a skeleton for models.py

---
+++ models.py
@@ -0,0 +1,35 @@
+from config import db
+
+
+'''
+class Teams(db.Model):
+    team = 
+    division = 
+    city = 
+    state = 
+    stadium = 
+    coach = 
+    year_established = 
+
+
+
+class Players(db.Model):
+    player = 
+    number = 
+    team = 
+    position = 
+    college = 
+    height = 
+    weight = 
+    seasons_played = 
+
+
+
+class Divisions(db.Model):
+    division = 
+    conference = 
+    year_founded = 
+    most_recent_champion = 
+    most_division_titles_team = 
+    most_division_titles_amount = 
+'''
