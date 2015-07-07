d739bdbc2582c5fb694213b6ff752204de82d27e
saNed <this.is.saned@gmail.com>
2015-07-06 20:05:05 -0500

    removed indentation

--- manage.py
+++ manage.py
@@ -11,17 +11,17 @@ if __name__ == "__main__":
 
 
 
-    envpath = '/root/mv-venv/local/lib/python2.7/site-packages'
+envpath = '/root/mv-venv/local/lib/python2.7/site-packages'
 
-	# we add currently directory to path and change to it
-	pwd = os.path.dirname(os.path.abspath(__file__))
-	os.chdir(pwd)
-	sys.path = [pwd] + sys.path
+# we add currently directory to path and change to it
+pwd = os.path.dirname(os.path.abspath(__file__))
+os.chdir(pwd)
+sys.path = [pwd] + sys.path
 
-	# Append paths
-	site.addsitedir(envpath)
+# Append paths
+site.addsitedir(envpath)
 
-	# now start django
-	from django.core.handlers.wsgi import WSGIHandler
-	#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-	application = WSGIHandler()
+# now start django
+from django.core.handlers.wsgi import WSGIHandler
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+application = WSGIHandler()
