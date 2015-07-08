87e1f9d28c4e3bfc3d24041294f98c0816beeca7
saNed <this.is.saned@gmail.com>
2015-07-07 19:49:04 -0500

    removed erranious import

--- yafootballdb/yafootballdb/views.py
+++ yafootballdb/yafootballdb/views.py
@@ -3,11 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-from django.template import RequestContext, loader
-
-from .models import Question
-from django.http import Http404
-from django.shortcuts import get_object_or_404
+from django.template import loader
 
 
 def home(request):
