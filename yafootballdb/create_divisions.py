775af4d4ea536bd8ccfcd3ce7271b5bca76ef225
saNed <this.is.saned@gmail.com>
2015-07-11 16:25:29 -0500

    created create_divisions.py

---
+++ yafootballdb/create_divisions.py
@@ -0,0 +1,16 @@
+# import json
+#import sys
+# import django
+from wc_app.models import *
+
+import 
+
+open ()
+teams_file = 
+teams_cache = teams_file.json()
+
+for key in adict:
+    q = 0
+    q =Country(country_name=key, country_code=adict[key][0], rank = adict[key][1], flag=adict[key][2], symbol_flag=adict[key][3], map_url=adict[key][4], team_logo_url=adict[key][5], team_video_url=adict[key][6])
+    q.save()
+# save()
