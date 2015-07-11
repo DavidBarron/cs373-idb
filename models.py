3f95f6f0ecd2a1e47cbcbb541dc7221e3e6a997f
DavidBarron <david.barron91@gmail.com>
2015-07-11 13:25:21 -0500

    Stuff...

--- models.py
+++ models.py
@@ -9,14 +9,14 @@ class Division(models.Model):
     The __str__ function returns the division name as a string.
     """
     division = models.CharField(max_length=200)
-    division_image = models.CharField(max_length=200)
+    dimage = models.CharField(max_length=200)
     conference = models.CharField(max_length=200)
-    league = models.CharField(max_length=200)
-    sport = models.CharField(max_length=200)
+    cimage = models.CharField(max_length=200)
     founded = models.CharField(max_length=200)
-    teams = models.ForeignKey(Team)
-    most_recent_champ = models.ForeignKey(Team)
-    most_champs = models.ForeignKey(Team)
+    rchamp = models.CharField(max_length=200)     # most recent champion
+    mchamps = models.CharField(max_length=200)    # most championships
+    cnum = models.CharField(max_length=200)  # that number of champs
+    # teams
 
     def __str__(self) :
         return self.division
@@ -30,16 +30,16 @@ class Team(models.Model):
     The __str__ function returns the team name as a string.
     """    
     team = models.CharField(max_length=200)
-    team_image = models.CharField(max_length=400)     
-    division = models.ForeignKey(Division) 
+    timage = models.CharField(max_length=400)     
     state = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
     stadium = models.CharField(max_length=200)
-    stadium_image = models.DateField()
-    coach = models.CharField(max_length=200)     
+    simage = models.DateField()
+    coach = models.CharField(max_length=200)   
     established = models.CharField(max_length=200)
-    conference_champs = models.CharField(max_length=200)
-    superbowl_champs = models.CharField(max_length=200)
+    cchamps = models.CharField(max_length=200)
+    schamps = models.CharField(max_length=200)
+    #players
 
 
     def __str__(self) :
@@ -61,7 +61,7 @@ class Player(models.Model):
     age = models.FloatField()
     experience = models.CharField(max_length=200)
     college = models.CharField(max_length=200)
-    player_image = models.CharField(max_length=400)
+    pimage = models.CharField(max_length=400)
 
     def __str__(self) :
         return self.name
